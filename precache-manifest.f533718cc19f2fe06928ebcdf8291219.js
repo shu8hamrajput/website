d85bab062048c7532abb5421a3086025d505c0b2
shubham <shu8hamrajput@gmail.com>
2020-04-23 04:45:54 +0530

    Updates

---
+++ precache-manifest.f533718cc19f2fe06928ebcdf8291219.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac4465d84010f5d16fa39045e196096",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "4aa7c780223a9a745694",
+    "url": "/website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "79db7d4981814e0cb005",
+    "url": "/website/static/js/2.455f1b76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website/static/js/2.455f1b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aa7c780223a9a745694",
+    "url": "/website/static/js/main.240b81b9.chunk.js"
+  },
+  {
+    "revision": "a50d945060eddc6481b5",
+    "url": "/website/static/js/runtime-main.e471a7fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
